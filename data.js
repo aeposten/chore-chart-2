184f53c4c32e38b9ada6f3fd9d76a4c01adfaa04
Amy <86963506+aeposten@users.noreply.github.com>
2022-12-01 11:54:10 -0800

    Basic HTML, CSS, and JS files added

---
+++ data.js
@@ -0,0 +1,26 @@
+export const chores = [
+    {
+        name: "No Accidents All Day",
+        count: 0,
+    },
+    {
+        name: "Finish Lunch at Daycare",
+        count: 0,
+    },
+    {
+        name: "Stay in Your Bed All Night",
+        count: 0,
+    },
+    {
+        name: "Feed Puppy and Kitty",
+        count: 0,
+    },
+    {
+        name: "Try a New Food",
+        count: 0,
+    },
+    {
+        name: "Read for 20 Minutes",
+        count: 0,
+    },
+]
