153f587160b3ae418c6ac044282a38151c32b3dc
Amy <86963506+aeposten@users.noreply.github.com>
2022-12-02 11:00:39 -0800

    Implemented timer and commented JS

--- index.js
+++ index.js
@@ -1,10 +1,39 @@
 import { chores } from "./data.js";
+
+// Global variables for chore counters
 const CHORES = chores;
-const choresEl = document.getElementById("all-chores");
-const totalCountEl = document.getElementById("total-count");
-const prizeEl = document.getElementById("get-prize");
+const choresEl = selectComponent("all-chores");
+const totalCountEl = selectComponent("total-count");
+const prizeEl = selectComponent("get-prize");
 
 renderChoreHTML();
+
+// Global variables to initialize timer
+let timer;
+const timerEl = selectComponent("timer");
+
+renderTimerHTML();
+
+// Global variables for timer elements
+const timerSpan = selectComponent("time");
+const startBtn = selectComponent("start-timer");
+const pauseBtn = selectComponent("pause-timer");
+const resetBtn = selectComponent("reset-timer");
+
+// Global variables for minutes and seconds
+let initialMinutes = 20;
+let totalSeconds = initialMinutes * 60;
+
+// Is timer paused?
+let paused = true;
+
+
+
+//Gets component by id
+function selectComponent(elementId) {
+  let component = document.getElementById(elementId);
+  return component;
+}
 
 document.addEventListener("click", function (e) {
   if (e.target.dataset.increment) {
@@ -14,6 +43,7 @@ document.addEventListener("click", function (e) {
   }
 });
 
+// Renders chore HTML
 function renderChoreHTML() {
   const choreDivs = CHORES.map(
     ({ chore, name, count }, index) =>
@@ -25,6 +55,7 @@ function renderChoreHTML() {
   return (choresEl.innerHTML = choreDivs.join(""));
 }
 
+// Increments chore counter
 function incrementCount(choreIndex) {
   let choreCount = CHORES[choreIndex].count;
 
@@ -33,6 +64,7 @@ function incrementCount(choreIndex) {
   updateChoreCount(choreIndex, choreCount);
 }
 
+// Resets chore counter
 function resetCount(choreIndex) {
   let choreCount = CHORES[choreIndex].count;
 
@@ -40,14 +72,16 @@ function resetCount(choreIndex) {
   updateChoreCount(choreIndex, choreCount);
 }
 
+// Used for updating chore count in increment and reset functions
 function updateChoreCount(choreIndex, choreCount) {
-  const choreCountSpan = document.getElementById(`count-${choreIndex}`);
+  const choreCountSpan = selectComponent(`count-${choreIndex}`);
 
   choreCountSpan.textContent = choreCount;
   CHORES[choreIndex].count = choreCount;
   calculateTotal(CHORES);
 }
 
+// Calculates total chores completed
 function calculateTotal(choreArr) {
   let total = 0;
 
@@ -61,5 +95,66 @@ function calculateTotal(choreArr) {
     prizeEl.classList.add("display-none");
   } else if (total >= 15) {
     prizeEl.classList.remove("display-none");
+  }
+}
+
+// Renders HTML for timer
+function renderTimerHTML() {
+  timerEl.innerHTML = `
+  <h3>Chore Timer</h3>
+  <div>
+      <span id="time">20:00</span><button class="btn" id="start-timer">Start Timer</button>
+      <button class="btn" id="pause-timer">Pause Timer</button><button class="btn" id="reset-timer">Reset Timer</button>
+  </div>
+  `;
+}
+
+// Sets initial time calculations
+function setTimeCalculations() {
+  let minutes = Math.floor(totalSeconds / 60);
+  let seconds = totalSeconds % 60;
+
+  timerSpan.textContent = `${minutes}:${(seconds =
+    seconds < 10 ? "0" + seconds : seconds)}`;
+}
+
+//Starts Timer
+function startTimer() {
+  if (paused) {
+    paused = false;
+    timer = setInterval(countdown, 1000);
+  }
+}
+
+startBtn.addEventListener("click", startTimer);
+
+//Pauses Timer
+function pauseTimer() {
+  paused = true;
+  if (timer) {
+    clearInterval(timer);
+  }
+}
+
+pauseBtn.addEventListener("click", pauseTimer);
+
+//Resets Timer
+function resetTimer() {
+  paused = true;
+  clearInterval(timer);
+  totalSeconds = initialMinutes * 60;
+  timerSpan.textContent = `${initialMinutes}:00`;
+}
+
+resetBtn.addEventListener("click", resetTimer);
+
+//Starts Countdown
+function countdown() {
+  totalSeconds--;
+  setTimeCalculations();
+
+  if (totalSeconds === 0) {
+    paused = true;
+    clearInterval(timer);
   }
 }
